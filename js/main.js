64834c1c73d9e5bac0059aec153a0e4faf640ac8
jaafar2000 <jaafar200021@gmail.com>
2022-04-23 23:02:32 +0530

    js edit

--- js/main.js
+++ js/main.js
@@ -18,19 +18,3 @@ img.addEventListener("click",function(){
 })
 
 
-
-// img.addEventListener("click",function(){
-//     if (img.classList.contains("H__menu")){
-//         img.classList.replace("H__menu","c__menu");
-//         if (mobileMenu.classList.contains("hide")){
-//             mobileMenu.classList.replace("hide","show")
-//         }else {
-//             mobileMenu.classList.add("show")
-//         }
-
-
-//     }else if(img.classList.contains("c__menu")){
-//         img.classList.replace("c__menu","H__menu");
-//         mobileMenu.classList.replace("show","hide")
-//     }
-// })
